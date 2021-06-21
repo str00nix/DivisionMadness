e7340199521b62bee043e1e9279cf04a14a95248
str00nix <zivkosh@yahoo.com>
2021-06-21 23:47:35 +0200

    grouping lists of divisors by length

--- divmadness.py
+++ divmadness.py
@@ -1,5 +1,6 @@
 def main():
 	maxnum = 1000
+	maxnumdiglen = len(str(maxnum))
 	
 	d = {}
 	
@@ -8,10 +9,18 @@ def main():
 		if ar != []:
 			d[i] = ar
 	
+	
 	d = {k: v for k, v in sorted(d.items(), key=lambda x: (len(x[1]),x[0]),reverse=True)}
 	
 	for a in d.items():
 		print(f"{a[0]} = {a[1]}")
+	
+	print()
+	
+	divisorGroupsByLength = {len(v): [i for i in d.values() if len(v) == len(i)] for k, v in d.items()}
+	
+	for a in divisorGroupsByLength.items():
+		print(f"{a[0]}: {a[1]}\n")
 
 if __name__ == '__main__':
 	main()
